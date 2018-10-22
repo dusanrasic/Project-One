be21c03984ea4bbfe491bb00b4aa2b62bc086abf
Dusan Rasic <dusanrasic@spinviewglobal.com>
2018-10-22 13:51:26 +0200

    api solved

--- src/components/Items.js
+++ src/components/Items.js
@@ -13,30 +13,100 @@ class Items extends Component{
 		
 	}
 	componentDidMount(){
-		const dataToken = parseInt(localStorage.getItem('token'));
-		const tokenData = api.get('api/token');
-		if(localStorage.getItem('token') == null){
-			tokenData.then(
+		const dataToken = localStorage.getItem('token');
+		const tokenData = api.get('api/token')
+			.then( 
 				(res) => {
-					token.addItemToLocalStorage(res.token);
-					console.log(res);
-					console.log(localStorage.getItem('token'));
+					if (dataToken == null || dataToken !== res.token){
+						token.addItemToLocalStorage(res.token);
+						// console.log(res);
+						// console.log(dataToken);
+						getData();
+					} else if(dataToken === res.token){
+						getData();
+					}
 				}
 			);
-		} else{
-			const data = api.get('api/data', {
-				params: {
+			
+		function getData(){
+			if(dataToken !== null){
+				const data = api.get('api/data', {
 					from: 1,
 					to: 20,
-					token: dataToken,
-				}
-			})
-			.then(
-				(res)=>{
-					console.log(res);
-				}
-			);
+					token: dataToken
+				})
+					.then(
+					(res)=>{
+						const {data} = res;
+						let {index, slot, city, velocity} = data;
+						// let index;
+						// let slot;
+						// let city;
+						// let velocity;
+
+						console.table(data)
+						let items = res.results.map((item) => {
+							index = item.index
+							if(item.slot === null){
+								slot = 0;
+							} else{
+								slot = item.slot;
+							}
+							if(item.city === null){
+								city = "None";
+							} else{
+								city = item.city;
+							}
+							if(item.velocity === null){
+								velocity = 0.00;
+							} else{
+								velocity = item.velocity;
+							}
+							return (
+								<tr>
+									<td>`${index}`</td>
+									<td>`${slot}`</td>
+									<td>`${city}`</td>
+									<td>`${velocity}`</td>
+								</tr>
+							)
+						})
+						});
+						
+						// data.forEach(element => {
+						// 	console.log(element.index);
+						// 	console.log(element.slot);
+						// 	console.log(element.city);
+						// 	console.log(element.velocity);
+						// 	index = element.index;
+						// 	if(element.slot === null){
+						// 		slot = 0;
+						// 	} else{
+						// 		slot = element.slot;
+						// 	}
+						// 	if(element.city === null){
+						// 		city = "None";
+						// 	} else{
+						// 		city = element.city;
+						// 	}
+						// 	if(element.velocity === null){
+						// 		velocity = 0.00;
+						// 	} else{
+						// 		velocity = element.velocity;
+						// 	}
+						// 	return (
+						// 		<tr>
+						// 			<td>`${index}`</td>
+						// 			<td>`${slot}`</td>
+						// 			<td>`${city}`</td>
+						// 			<td>`${velocity}`</td>
+						// 		</tr>
+						// 	)
+						// });
+						
+			}
 		}
+		
 		
 		
 		// fetch(this.base_url)
@@ -58,18 +128,27 @@ class Items extends Component{
 		// 	});
 		// });
 	}
+	
 	render(){
 		return(
 			<table>
-			<tr>
-				<th>Index</th>
-				<th>Slot</th> 
-				<th>City</th>
-				<th>Velocity</th>
-			</tr>
-			{this.state.items}
+				<thead>
+					<tr>
+						<th>Index</th>
+						<th>Slot</th>
+						<th>City</th>
+						<th>Velocity</th>
+					</tr>
+				</thead>
+				<tbody>
+					
+				</tbody>
+				<tfoot>
+					<tr>
+						<td colspan="2">React project</td>
+					</tr>
+				</tfoot>
 			</table>
-			
 		);
 	}
 }
